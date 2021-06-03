306bbbe5ed7e3053509e85454f4fe8ada5de5cd1
Nada16 <n.al-noory@hotmail.com>
2021-06-03 15:35:35 +0300

    Adding HelloWorld project
    HelloWorld project with JUnit

---
+++ HelloWorld/src/PrintHelloWorld.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PrintHelloWorld {
+	@Test
+	public void printHelloWorld() {
+		assertEquals(0, 0);
+		System.out.println("HelloWorld!");
+
+	}
+}
