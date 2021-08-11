3aa361d56dff30e944c637557eea0ac378d75a4c
Nada16 <n.al-noory@hotmail.com>
2021-08-11 13:41:02 +0300

    Adding comments to twitter log in

--- Appium_Mobile_Testing/src/main/java/Appium/TwitterLogin.java
+++ Appium_Mobile_Testing/src/main/java/Appium/TwitterLogin.java
@@ -27,35 +27,32 @@ public class TwitterLogin {
 		capabilities.setCapability(AndroidMobileCapabilityType.VERSION, "11");
 		//Device Name
 		capabilities.setCapability("deviceName", "sdk_gphone_x86");
-
 		//To open the browser
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
 
 		//Create RemoteWebDriver instance and connect to the Appium server
-		//It will launch the Clock Application in Android Device using Desired Capabilities configurations
 		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 	}
 
 	@Test
 	public void testcase1(){
-		//Navigate to Google and search for "Coding Dojo"
+		//Navigate to Twitter
 		driver.get("https://www.twitter.com/");
-
+		
+		//click on Not now when it asks to download twitter app
 		WebDriverWait wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Not now']"))).click();
-
+		//click on log in and choose "use phone, email or username"
 		driver.findElementByXPath("//span[text()='Log in']").click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Use phone, email or username']"))).click();
-		
+		//fill email, and password and click log in
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='session[username_or_email]']"))).sendKeys("email@email.com");
 		driver.findElementByXPath("//input[@name='session[password]']").sendKeys("123456");
 		driver.findElementByXPath("//span[text()='Log in']").click();
-
+		//fill phone number, and password and click log in
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='session[username_or_email]']"))).sendKeys("0555555555");
 		driver.findElementByXPath("//input[@name='session[password]']").sendKeys("123456");
 		driver.findElementByXPath("//span[text()='Log in']").click();
-
-
 	}
 
 	@AfterTest
