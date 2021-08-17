96ea954a42225df1e4c5a3160af41f1e4e73ab59
Nada16 <n.al-noory@hotmail.com>
2021-08-17 11:40:25 +0300

    Mini Project to order items from noon using appium

--- Appium_Mobile_Testing/src/main/java/Appium/Noon.java
+++ Appium_Mobile_Testing/src/main/java/Appium/Noon.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -40,39 +41,36 @@ public class Noon {
 
 	@Test
 	public void testcase1()throws Exception {
-
+		//Go to noon.com
 		WebDriverWait wait = new WebDriverWait(driver,20);
 		driver.get("https://www.noon.com/saudi-en/");
 		Thread.sleep(500);
 
-		//Electronics category
+		//Add an item from "Electronics" category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//span[text()='Categories']/parent::div")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[text()='Electronics']")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//p[text()='Gaming']")))).click();
 		addFirstItemToCart(wait);
 
-		//Beauty category
+		//Add an item from "Beauty" category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//span[text()='Categories']/parent::div")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[text()='Beauty & Personal Care']")))).click();
 		addFirstItemToCart(wait);
 
-
-		//Groceries category
+		//Add an item from "Groceries" category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//span[text()='Categories']/parent::div")))).click();
 		WebElement element =  wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[text()='Grocery']"))));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
 		element.click();
 		addFirstItemToCart(wait);
 
-
-		//Home category
+		//Add an item from "Home" category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//span[text()='Categories']/parent::div")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[text()='Home & Kitchen']")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//p[text()='Home Decor']")))).click();
 		addFirstItemToCart(wait);
 
-
-		//School category
+		//Add an item from "School" category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//span[text()='Back to School']/parent::div")))).click();
 		element =  wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@class='sc-1xjgu8-2 lgLLLe']"))));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
@@ -82,15 +80,16 @@ public class Noon {
 		//Go to cart and Checkout 
 		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//button[@class='sc-1g6zspo-8 bswpPM']")))).click();
 		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//div[@class='sc-1mdgs01-2 bydguV']/button")))).click();
+		//Sign in
 		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//input[@name='email']")))).sendKeys("doyana3341@5ubo.com"+Keys.TAB+"Tt123456"+Keys.ENTER);
-		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//input[@id='searchBox']")))).sendKeys("Riyadh"+Keys.ENTER);
-		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//button[@aria-label='Confirm location']")))).click();
-		
-
-		
+		//Confirm address
+		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//button[@aria-label='Continue']")))).click();
+		String result = wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//div[@class='jsx-2109631596 phoneVerificationContainer']/h2")))).getText();
+		Assert.assertEquals(result, "Verify Phone Number");
 	}
 
 	public void addFirstItemToCart(WebDriverWait wait) {
+		//Click on the first item in a category
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@class='sc-1x4gzd-1 laCZzz']")))).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@class='sc-14wnt1u-0 hxqhii grid']")))).click();
 		wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//div[@class='hp0ehb-1 KWRMQ']/div[@class='cart-button']")))).click();
@@ -100,7 +99,7 @@ public class Noon {
 	@AfterTest
 	public void testCaseTearDown() throws InterruptedException {
 		//Stop the test
-		//		Thread.sleep(2000);
-		//		driver.quit();
+		Thread.sleep(2000);
+		driver.quit();
 	}
 }
